eb4cd17e8f061550fa53bfc5bf9600188ccb7dc5
sreelakshmyprithviraj-prog <sreelakshmyprithviraj@gmail.com>
2026-04-12 20:02:10 +0530

    feat: add movie.py to load rental shop dataset via pandas

---
+++ movie.py
@@ -0,0 +1,19 @@
+import pandas as pd
+actor=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\actor.csv")
+address=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\address.csv")
+category=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\category.csv")
+city=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\city.csv")
+country=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\country.csv")
+customer=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\customer.csv")
+film_actor=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\film_actor.csv")
+film_category=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\film_category.csv")
+film=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\film.csv")
+inventory=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\inventory.csv")
+language=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\language.csv")
+payment=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\payment.csv")
+rental=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\rental.csv")
+staff=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\staff.csv")
+store=pd.read_csv(r"C:\Users\User\Documents\Malu\Python\Movie_rental_shop\store.csv")
+
+
+
